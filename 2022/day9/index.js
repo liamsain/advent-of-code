21b9844d188bf15a0104740f0d855b29a13a8ce4
liamsain <liamkennedy89@outlook.com>
2022-12-10 23:15:02 +0000

    day 9 part 1 correct

--- 2022/day9/index.js
+++ 2022/day9/index.js
@@ -6,7 +6,7 @@ R 4
 D 1
 L 5
 R 2`;
-
+const prompt = require('prompt-sync')({sigint: true});
 const fs = require('fs');
 let input;
 try {
@@ -16,10 +16,17 @@ try {
 }
 const splitInput = input.split('\n');
 
+const gridBoundaries = [0, 0];
+let gridXLeft = 0;
+let gridXRight = 0;
+let gridYTop = 0;
+let gridYBottom = 0;
+
 const origin = [0, 0];
 const hPos = [0, 0];
 const tPos = [0, 0]
 const tPositions = [[0, 0]]; 
+
 const moveCoord = (coord, direction) => {
 	if (direction == 'R') {
 		coord[0] += 1;
@@ -31,13 +38,60 @@ const moveCoord = (coord, direction) => {
 		coord[1] += 1;
 	}
 };
+
+// for debugging
+function updateDrawnGrid() {
+  if (hPos[0] > gridXRight) {
+    gridXRight = hPos[0];
+  } else if (hPos[0] < gridXLeft) {
+    gridXLeft = hPos[0];
+  }
+  if (hPos[1] > gridYTop) {
+    gridYTop = hPos[1];
+  } else if (hPos[1] < gridYBottom) {
+    gridYBottom = hPos[1];
+  }
+  const grid = [];
+  for (let row = gridYTop; row >= gridYBottom; row--) {
+    let line = [];
+    for (let col = gridXLeft; col <= gridXRight; col++) {
+      const hAndTMatch = hPos[0] == tPos[0] && hPos[1] == tPos[1];
+      if (hPos[0] == col && hPos[1] == row) {
+        line.push('H ');
+        continue;
+      }
+      if (tPos[0] == col && tPos[1] == row) {
+        if (!hAndTMatch) {
+          line.push('T ');
+          continue;
+        }
+      }
+      line.push('. ');
+    }
+    grid.push([...line]);
+    line = [];
+  }
+  // draw it
+  grid.forEach(x => {
+    console.log(x.join(''))
+  })
+  // prompt('>');
+}
+
+console.time('fun');
 for (let commandIndex = 0; commandIndex < splitInput.length; commandIndex++) {
 	const row = splitInput[commandIndex]
 	const direction = row[0];
 	const amount = Number(row.split(' ')[1]);
 	for (let i =0; i < amount; i++) {
+    /*
+    console.clear();
+    console.info(`Executing: ${row}`); 
+    console.info(`Next: ${splitInput[commandIndex + 1]}`); 
+    */
+
 		moveCoord(hPos, direction);
-    const hx = hPos[0];
+    const hx = hPos[0]
     const hy = hPos[1];
     const tx = tPos[0];
     const ty = tPos[1];
@@ -79,27 +133,19 @@ for (let commandIndex = 0; commandIndex < splitInput.length; commandIndex++) {
         console.error("you didn't account for this case you silly billy");
       }
 
-      tPositions.push([...tPos]);
+      addCoordToTPos([...tPos]);
     }
+    // updateDrawnGrid();
 	}
 }
-
-console.log(tPositions[0]);
-function getUniqueCoordsCount(coords) {
-  let result = 0;
-  for (let i = 0; i < coords.length; i++) {
-    let current = coords[i];
-    let arr = coords.filter((val, ci) => ci !== i);
-    const duplicate = arr.find(x => x[0] == current[0] && x[1] == current[1]);
-    if (!duplicate) {
-      result += 1;
-    }
+function addCoordToTPos(coord) {
+  const existing = tPositions.find(x => x[0] == coord[0] && x[1] == coord[1]);
+  if (!existing) {
+    tPositions.push([...coord]);
   }
-  return result;
 }
-// 4336 too low
-console.time('hi');
-console.log('unique tail positions count:', getUniqueCoordsCount(tPositions));
-console.timeEnd('hi');
 
+console.log('unique tail positions count:', tPositions.length);
+
+console.timeEnd('fun');
 
