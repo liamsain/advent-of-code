b2335e50719754b46dd7685eee562539e9feebce
liamsain <liamkennedy89@outlook.com>
2022-12-11 08:27:55 +0000

    remove input from day 10 index

--- 2022/day10/index.js
+++ 2022/day10/index.js
@@ -1,153 +1,8 @@
 const fs = require('fs');
-let testInput = `addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop`;
+
 let input;
 try {
-  input = fs.readFileSync('./input.txt', 'utf8');
+  input = fs.readFileSync('./testInput.txt', 'utf8');
 } catch(err) {
   console.error(err);
 }
@@ -161,7 +16,8 @@ function cycleNumberCheck() {
     signalStrengthsSum += cycleNumber * regVal;
   }
 }
-const splitInput = testInput.split('\n');
+const splitInput = input.split('\n');
+console.time('fun');
 for (let i = 0; i < splitInput.length;i++) {
   cycleNumberCheck();
   const instruction = splitInput[i].split(' ')[0];
@@ -170,10 +26,12 @@ for (let i = 0; i < splitInput.length;i++) {
     cycleNumber += 1;
     cycleNumberCheck();
     cycleNumber += 1;
-    cycleNumberCheck();
     regVal = regVal + Number(operand);
-  } 
-  cycleNumber += 1;
+    cycleNumberCheck();
+  }  else {
+    cycleNumber += 1;
+  }
 }
+console.timeEnd('fun');
 // test answer is 13140
 console.log('signal strength sum: ', signalStrengthsSum);
