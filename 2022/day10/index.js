6e9b6c2ae9b2ebf4844856b2fb73ebc64e132ed0
liamsain <liamkennedy89@outlook.com>
2022-12-11 20:09:35 +0000

    finish day 10

--- 2022/day10/index.js
+++ 2022/day10/index.js
@@ -10,15 +10,32 @@ try {
 let regVal = 1;
 let cycleNumber = 1;
 let signalStrengthsSum = 0;
+let currentDrawIndex = 0;
+
 const importantCycleNumbers = [20, 60, 100, 140, 180, 220];
+const crt = new Array(6).fill(new Array(40).fill('.')).map(line => [...line]);
+
 function cycleNumberCheck() {
   if (importantCycleNumbers.includes(cycleNumber)) {
     signalStrengthsSum += cycleNumber * regVal;
   }
+  let pixel = '.'
+  const row = Math.floor(currentDrawIndex / 40);
+  const col = currentDrawIndex % 40;
+  const currentRegVal = regVal + (row * 40);
+  const overlap = currentDrawIndex == currentRegVal - 1 || currentDrawIndex == currentRegVal || currentDrawIndex == currentRegVal + 1;
+  if (overlap) {
+    pixel = '#';
+  }
+  crt[row][col] = pixel;
+  currentDrawIndex += 1;
+
 }
 const splitInput = input.split('\n');
 console.time('fun');
+
 for (let i = 0; i < splitInput.length;i++) {
+
   cycleNumberCheck();
   const instruction = splitInput[i].split(' ')[0];
   const operand = splitInput[i].split(' ')[1];
@@ -30,8 +47,11 @@ for (let i = 0; i < splitInput.length;i++) {
   }  else {
     cycleNumber += 1;
   }
+
 }
 console.timeEnd('fun');
-// test answer is 13140
-const testAnswer = 13140;
 console.log('signal strength sum: ', signalStrengthsSum);
+
+console.log(crt.forEach(row => {
+  console.log(row.join(''));
+}));
